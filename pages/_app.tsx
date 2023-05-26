1a82cd98e09e4352a34cfea6a1eada4bef126748
sergeycode <s_ovcharenko@outlook.com>
2023-05-26 09:14:36 -0400

    cleanup theme, add firebase config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,36 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'styles/theme';
+import { FirebaseAppProvider } from 'reactfire';
+import { initializeApp } from 'firebase/app';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+if (process.env.NODE_ENV === 'development') {
+    console.log('Running in development mode');
+    connectFirestoreEmulator(db, 'localhost', 8080);
+    connectAuthEmulator(auth, 'http://localhost:9099/');
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+    return (
+        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+            <ChakraProvider theme={theme}>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </FirebaseAppProvider>
+    );
 }
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,114 +1,17 @@
 import { extendTheme } from '@chakra-ui/react';
-import { Raleway, Jost } from '@next/font/google';
+import { Inter } from '@next/font/google';
 
-const raleway = Raleway({ subsets: ['latin'], display: 'swap' });
-const jost = Jost({ subsets: ['latin'], display: 'swap' });
-
-const customButtonProps = {
-  fontFamily: 'heading',
-  fontSize: 'sm',
-  borderRadius: '0',
-  minH: '42px',
-  fontWeight: 'medium',
-  textTransform: 'uppercase',
-  transition: 'all .2s ease-in-out',
-};
+const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
 const theme = extendTheme({
   fonts: {
-    heading: jost.style.fontFamily,
-    body: raleway.style.fontFamily,
-  },
-  colors: {
-    transparent: 'transparent',
-    // colors based on design
-    brand: {
-      main: '#5990dc',
-    },
-    accent: {
-      main: '#41a785',
-    },
-    ui: {
-      primary: '#181819',
-    },
-    text: {
-      primary: '#181819',
-      secondary: '#41a785',
-    },
-    bg: {
-      primary: '#F1F1F1',
-      secondary: '#EFF6FF',
-    },
-    // shades with Tailwind Shades plugin where 500 is the main/primary color
-    blue: {
-      100: '#dee9f8',
-      200: '#bdd3f1',
-      300: '#9bbcea',
-      400: '#7aa6e3',
-      500: '#5990dc',
-      600: '#4773b0',
-      700: '#355684',
-      800: '#243a58',
-      900: '#121d2c',
-    },
-    teal: {
-      100: '#d9ede7',
-      200: '#b3dcce',
-      300: '#8dcab6',
-      400: '#67b99d',
-      500: '#41a785',
-      600: '#34866a',
-      700: '#276450',
-      800: '#1a4335',
-      900: '#0d211b',
-    },
-    black: {
-      100: '#d1d1d1',
-      200: '#a3a3a3',
-      300: '#747475',
-      400: '#464647',
-      500: '#181819',
-      600: '#131314',
-      700: '#0e0e0f',
-      800: '#0a0a0a',
-      900: '#050505',
-    },
-  },
-  styles: {
-    global: {
-      body: {
-        color: 'text.primary',
-      },
-      p: {
-        color: 'text.primary',
-      },
-    },
+    heading: inter.style.fontFamily,
+    body: inter.style.fontFamily,
   },
   components: {
     Container: {
       baseStyle: {
-        maxW: ['100%', '540px', '720px', '960px', '1140px', '1280px'],
-        px: { base: 8, xl: 0 },
-      },
-    },
-    Button: {
-      variants: {
-        primary: {
-          ...customButtonProps,
-          bgColor: 'ui.primary',
-          color: 'white',
-          _hover: {
-            background: 'black.400',
-          },
-        },
-        secondary: {
-          ...customButtonProps,
-          color: 'white',
-          bgColor: 'brand.main',
-          _hover: {
-            background: 'blue.600',
-          },
-        },
+        maxW: ['100%', '540px', '720px', '960px', '1140px'],
       },
     },
   },
@@ -118,7 +21,6 @@ const theme = extendTheme({
     lg: '992px',
     xl: '1200px',
     '2xl': '1400px',
-    '3xl': '1600px',
   },
 });
 
